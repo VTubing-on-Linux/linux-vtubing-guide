130d2dd2388a8250bff38236952a1ed0d44920db
h-banii <121690516+h-banii@users.noreply.github.com>
2025-09-17 03:52:53 +0000

    nix: Add basic webserver

--- flake.nix
+++ flake.nix
@@ -20,6 +20,34 @@
         default = pkgsFor.${system}.callPackage ./nix/pkgs/linux-vtubing-guide { };
       });
 
+      legacyPackages = forAllSystems (
+        system:
+        let
+          base = "linux-vtubing-guide";
+        in
+        with pkgsFor.${system};
+        {
+          webserver = symlinkJoin {
+            name = "${base}-webserver";
+            meta.mainProgram = "http-server";
+            nativeBuildInputs = [ makeWrapper ];
+            paths = [
+              http-server
+              (linkFarm "" [
+                {
+                  name = "public/${base}";
+                  path = self.packages.${system}.default;
+                }
+              ])
+            ];
+            postBuild = ''
+              wrapProgram $out/bin/http-server \
+                --add-flags "$out/public"
+            '';
+          };
+        }
+      );
+
       devShells = forAllSystems (
         system: with pkgsFor.${system}; {
           default = mkShell {
