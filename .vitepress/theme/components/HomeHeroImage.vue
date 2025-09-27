9df0b7c3f83e00cdebce768b2aefd3a15a87b7ed
h-banii <121690516+h-banii@users.noreply.github.com>
2025-09-27 13:28:55 -0300

    .vitepress/theme: HomeHeroImage: Impl cycle manually to allow random cycle

--- .vitepress/theme/components/HomeHeroImage.vue
+++ .vitepress/theme/components/HomeHeroImage.vue
@@ -1,23 +1,51 @@
 <script setup>
 import { VPImage } from "vitepress/theme";
 import { useData } from "vitepress";
+import { onMounted, onUnmounted, ref } from "vue";
 
 const { frontmatter } = useData();
 
 const props = frontmatter.value.hero.carousel;
+
+function randomIndex() {
+  return ~~(Math.random() * (props.images?.length ?? 1));
+}
+
+function clampIndex(index) {
+  return index % (props.images?.length ?? 1);
+}
+
+const currentIndex = ref(props?.cycle?.random ? randomIndex() : 0);
+let slideInterval = -1;
+
+function startInterval() {
+  if (!props.cycle) return;
+
+  slideInterval = window.setInterval(() => {
+    currentIndex.value = props.cycle.random
+      ? randomIndex()
+      : clampIndex(currentIndex.value + 1);
+  }, Number(props.cycle.duration));
+}
+
+function clearInterval() {
+  window.clearInterval(slideInterval);
+}
+
+onMounted(startInterval);
+onUnmounted(clearInterval);
 </script>
 
 <template>
   <div v-if="props">
     <v-carousel
+      v-model="currentIndex"
       class="image-src"
       height="100%"
       hide-delimiters
-      :show-arrows="props['show-arrows']"
-      cycle
-      :interval="props.cycle.duration"
-      :crossfade="props.cycle.transition.type == 'crossfade'"
-      :transition-duration="props.cycle.transition.duration"
+      :show-arrows="props?.['show-arrows']"
+      :crossfade="props?.cycle?.transition?.type == 'crossfade'"
+      :transition-duration="props?.cycle?.transition?.duration"
     >
       <v-carousel-item v-for="image in props.images">
         <VPImage class="image-src" :image></VPImage>
