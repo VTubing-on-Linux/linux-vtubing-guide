40cb50459b41c3441bd3894f6cc0a61905b9998a
h-banii <121690516+h-banii@users.noreply.github.com>
2025-09-29 00:12:28 -0300

    .vitepress/theme: Add git, mastodon, and bluesky icons

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -6,7 +6,14 @@ import "./style.css";
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
-import { mdiYoutube, mdiTwitch, mdiTwitter, mdiLink } from "@mdi/js";
+import {
+  mdiYoutube,
+  mdiTwitch,
+  mdiTwitter,
+  mdiMastodon,
+  mdiGit,
+  mdiLink,
+} from "@mdi/js";
 import HomeHeroImage from "./components/HomeHeroImage.vue";
 import VTuberCard from "./components/VTuberCard.vue";
 
@@ -22,6 +29,11 @@ const vuetify = createVuetify({
       twitch: mdiTwitch,
       twitter: mdiTwitter,
       link: mdiLink,
+      mastodon: mdiMastodon,
+      git: mdiGit,
+      bluesky: [
+        "M5.202 1.422C7.954 3.488 10.913 7.677 12 9.924c1.087-2.248 4.046-6.436 6.798-8.502C20.783-.068 24-1.221 24 2.448c0 .733-.42 6.157-.667 7.038-.857 3.061-3.978 3.842-6.755 3.37 4.854.826 6.089 3.562 3.422 6.298-5.065 5.197-7.279-1.304-7.847-2.97-.104-.305-.153-.448-.153-.327-.001-.121-.049.021-.153.327C11.28 17.85 9.065 24.35 4 19.154c-2.667-2.736-1.432-5.473 3.422-6.299-2.777.473-5.899-.308-6.755-3.37C.42 8.605 0 3.181 0 2.448 0-1.221 3.217-.068 5.202 1.422",
+      ],
     },
     sets: {
       mdi,
