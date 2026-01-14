9604f378836b073d4fc24158eaa3f489e8b855c5
h-banii <121690516+h-banii@users.noreply.github.com>
2026-01-13 22:59:54 -0300

    en/config: Run formatter

--- src/en/config.ts
+++ src/en/config.ts
@@ -14,14 +14,20 @@ export default defineAdditionalConfig({
         text: "Getting Started",
         items: [
           { text: "Introduction", link: "/docs/docs" },
-          { text: "Getting Started", link: "/docs/getting_started/getting_started"},
+          {
+            text: "Getting Started",
+            link: "/docs/getting_started/getting_started",
+          },
           { text: "Installation", link: "/docs/getting_started/installation" },
         ],
       },
       {
         text: "Linux",
         items: [
-          { text: "Running windows software", link: "/docs/linux/running_win_software" },
+          {
+            text: "Running Windows software",
+            link: "/docs/linux/running_win_software",
+          },
           { text: "Wayland vs. X11", link: "/docs/linux/wayland_vs_xorg" },
           { text: "NixOS", link: "/docs/linux/nixos" },
         ],
@@ -30,7 +36,7 @@ export default defineAdditionalConfig({
         text: "Streaming",
         items: [
           { text: "OBS", link: "/docs/streaming/obs" },
-          { text: "Audio setup + tools", link: "/docs/streaming/audio" }
+          { text: "Audio setup + tools", link: "/docs/streaming/audio" },
         ],
       },
       {
@@ -41,7 +47,7 @@ export default defineAdditionalConfig({
         text: "VTubing Software",
         items: [
           { text: "2D VTubing", link: "/docs/vt/2d" },
-          { text: "3D VTubing", link: "/docs/vt/3d" }
+          { text: "3D VTubing", link: "/docs/vt/3d" },
         ],
       },
       {
@@ -49,7 +55,7 @@ export default defineAdditionalConfig({
         items: [
           { text: "Other useful software", link: "/docs/misc/other_software" },
           { text: "Contributors", link: "/docs/misc/contributors" },
-          { text: "VTubers on Linux", link: "/docs/linux/vtubers" }
+          { text: "VTubers on Linux", link: "/docs/linux/vtubers" },
         ],
       },
     ],
